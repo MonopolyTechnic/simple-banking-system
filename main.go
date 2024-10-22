02dc10ace9d79817a4f39e709cb708677c248d39
SharpBit <31069084+SharpBit@users.noreply.github.com>
2024-10-21 22:31:23 -0400

    connect front end with go backend

--- main.go
+++ main.go
@@ -13,14 +13,20 @@ func main() {
 	// Show file and line number in logs
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	// Env vars (for now)
 	hostname = "127.0.0.1"
 	port = 5000
 
+	// Serve static content
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	// Routes
 	http.HandleFunc("/", index)
+
 	log.Printf("Running on http://%s:%d (Press CTRL+C to quit)", hostname, port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello world</h1>")
+	http.ServeFile(w, r, "./templates/index.html")
 }
