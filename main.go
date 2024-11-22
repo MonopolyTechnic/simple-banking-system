a0e84587b76f6f3c0525423867afea6b9a176772
Aditya Bachina <aditya.bachina@gmail.com>
2024-11-22 05:54:22 -0500

    made requested changes

--- main.go
+++ main.go
@@ -422,7 +422,7 @@ func transfer(w http.ResponseWriter, r *http.Request){
 		).Scan(&name, &id)
 
 		if err != nil {
-			return fmt.Errorf("eInvalid email: %s", email)
+			return fmt.Errorf("Invalid email: %s", email)
 		}
 		rows, err := conn.Query(
 			context.Background(),
@@ -431,7 +431,7 @@ func transfer(w http.ResponseWriter, r *http.Request){
 		)
 
 		if err != nil {
-			return fmt.Errorf("eInvalid return from accounts: %s", email)
+			return fmt.Errorf("Invalid return from accounts: %s", email)
 		}
 		defer rows.Close()
 
@@ -441,15 +441,18 @@ func transfer(w http.ResponseWriter, r *http.Request){
 				Balance float64 `json:"Balance"`
 			}
 			if err := rows.Scan(&account.Number, &account.Balance); err != nil {
-				return fmt.Errorf("eError scanning account row: %v", err)
+				return fmt.Errorf("Error scanning account row: %v", err)
 			}
 			accounts = append(accounts, account)
 		}
 		if err := rows.Err(); err != nil {
-			return fmt.Errorf("eError while iterating over account rows: %v", err)
+			return fmt.Errorf("Error while iterating over account rows: %v", err)
 		}
 		return nil
 	})
+	if err != nil {
+		AddFlash(r, w, "e"+err.Error())
+	}
 	if (r.Method == http.MethodPost){
 
 		sourceAccount := r.FormValue("sourceAccount")
@@ -485,7 +488,7 @@ func transfer(w http.ResponseWriter, r *http.Request){
 				destinationAccount,
 			).Scan(&dbal)
 			if err != nil {
-				return fmt.Errorf("eDestination Account does not exist.")
+				return fmt.Errorf("Destination Account does not exist.")
 			}
 			err = conn.QueryRow(
 				context.Background(),
@@ -493,7 +496,7 @@ func transfer(w http.ResponseWriter, r *http.Request){
 				currb-amount, sourceAccount,
 			).Scan(&tmp)
 			if err != nil {
-				return fmt.Errorf("eBad Update: %v", err)
+				return fmt.Errorf("Bad Update: %v", err)
 			}
 			err = conn.QueryRow(
 				context.Background(),
@@ -501,7 +504,7 @@ func transfer(w http.ResponseWriter, r *http.Request){
 				dbal+amount, destinationAccount,
 			).Scan(&tmp)
 			if err != nil {
-				return fmt.Errorf("eBad Update: %v", err)
+				return fmt.Errorf("Bad Update: %v", err)
 			}
 			err = conn.QueryRow(
 				context.Background(),
@@ -522,7 +525,9 @@ func transfer(w http.ResponseWriter, r *http.Request){
 			return nil
 		})
 		if err != nil{
-			AddFlash(r, w, err.Error())
+			AddFlash(r, w, "e"+err.Error())
+		} else {
+			AddFlash(r, w, "sTransfer Success")
 		}
 		http.Redirect(w, r, "/transfer", http.StatusSeeOther)
 	}
